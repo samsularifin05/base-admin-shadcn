f840c30e2674939843b88dc8cf8ef1f647817aa5
samsularifin05 <samsul026@gmail.com>
2024-06-21 14:38:17 +0700

    update toast

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -13,24 +13,9 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({
-        id,
-        title,
-        position,
-        description,
-        action,
-        ...props
-      }) {
+      {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast
-            key={id}
-            {...props}
-            className={`fixed z-50 flex flex-col gap-2 ${
-              position?.includes("top")
-                ? "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px] "
-                : "bottom-4"
-            } ${position?.includes("right") ? "right-4" : "left-4"}`}
-          >
+          <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
