c62e112f54b4d86ba9eea08ac686bf4aaa940658
samsularifin05 <samsul026@gmail.com>
2024-11-28 15:30:39 +0700

    update reducer

--- src/reduxStore/reducers/formReducer.ts
+++ src/reduxStore/reducers/formReducer.ts
@@ -11,11 +11,22 @@ const formsSlice = createSlice({
     ) => {
       const { form, values } = action.payload;
 
-      const validValues = Object.keys(values).reduce(
-        (acc, key) => {
+      // const validValues = Object.keys(values).reduce(
+      //   (acc, key) => {
+      //     if (key in state[form]) {
+      //       acc[key as keyof FormStateReduxFom[T]] =
+      //         values[key as keyof FormStateReduxFom[T]];
+      //     }
+      //     return acc;
+      //   },
+      //   {} as Partial<FormStateReduxFom[T]>
+      // );
+
+      const validValues = Object.entries(values).reduce(
+        (acc, [key, value]) => {
           if (key in state[form]) {
             acc[key as keyof FormStateReduxFom[T]] =
-              values[key as keyof FormStateReduxFom[T]];
+              value as FormStateReduxFom[T][keyof FormStateReduxFom[T]];
           }
           return acc;
         },
