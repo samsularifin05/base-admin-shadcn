248c11a45639113c4f6145b99b740097aa3f6577
samsularifin05 <samsul026@gmail.com>
2025-02-13 09:34:59 +0700

    update example validasi date

--- src/pages/admin/login/validate/index.ts
+++ src/pages/admin/login/validate/index.ts
@@ -5,26 +5,26 @@ export const validLoginSchema = yup.object().shape({
   password: yup
     .string()
     .required('Password is required')
-    .min(6, 'Password must be at least 6 characters'),
+    .min(6, 'Password must be at least 6 characters')
 
-  date: yup
-    .mixed()
-    .transform((value) => {
-      // console.log(value);
-      if (typeof value === 'object' && value !== null && value.startDate) {
-        return {
-          startDate: new Date(value.startDate).toISOString().split('T')[0],
-          endDate: new Date(value.endDate).toISOString().split('T')[0]
-        };
-      }
-      return value;
-    })
-    .test('is-valid-date', 'Tanggal harus dalam format YYYY-MM-DD', (value) => {
-      if (typeof value === 'object' && value !== null) {
-        console.log(value, 'MASUK');
-        const regex = /^\d{4}-\d{2}-\d{2}$/;
-        return typeof value === 'string' && regex.test(value);
-      }
-    })
-    .optional()
+  // date: yup
+  //   .mixed()
+  //   .transform((value) => {
+  //     // console.log(value);
+  //     if (typeof value === 'object' && value !== null && value.startDate) {
+  //       return {
+  //         startDate: new Date(value.startDate).toISOString().split('T')[0],
+  //         endDate: new Date(value.endDate).toISOString().split('T')[0]
+  //       };
+  //     }
+  //     return value;
+  //   })
+  //   .test('is-valid-date', 'Tanggal harus dalam format YYYY-MM-DD', (value) => {
+  //     if (typeof value === 'object' && value !== null) {
+  //       console.log(value, 'MASUK');
+  //       const regex = /^\d{4}-\d{2}-\d{2}$/;
+  //       return typeof value === 'string' && regex.test(value);
+  //     }
+  //   })
+  //   .optional()
 });
